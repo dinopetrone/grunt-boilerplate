a62592750b0645287c59e3761a33a2bd8e9e38dd
Dino Petrone <dinopetrone@gmail.com>
2013-08-13 13:59:03 -0700

    redoing everything

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,140 +1,97 @@
-/*global module:false, __dirname:false, require:false, process:false*/
 var dust = require('dustjs-linkedin');
-dust.optimizers.format = function(ctx, node) { return node };
-//if `grunt production` was run, build according to production rules
-var isProduction = process.argv.indexOf('production') > 0;
-
-//similar to underscore's _.defaults()
-var defaults = function( obj, dflts ){
-	var result = {}, prop;
-	//put all defaults in first
-	for( prop in dflts ){
-		if( dflts.hasOwnProperty( prop ) ){
-			result[prop] = dflts[prop];
-		}
-	}
-	//override with any obj props
-	for( prop in obj ){
-		if( obj.hasOwnProperty( prop ) ){
-			result[prop] = obj[prop];
-		}
-	}
-	//return combined
-	return result;
-};
-
-
-var pages = {
-	'index': { title: 'Home' },
-};
-
-
-
-function makePage( name, variables ){
-	//clear the dust cache so files change during `watch`
-	dust.cache = {};
-	variables = defaults( variables, {
-		cache: false,
-		views: __dirname + '/app/templates',
-		title: '',
-		debug: !isProduction,
-		pretty: !isProduction
-	});
-	return {
-		src: 'app/templates/' + name + '.dust.html',
-		dest: 'app/' +name+ '.html',
-		engine: 'dust',
-		variables: variables
-	};
-}
+dust.optimizers.format = function(ctx, node) { return node; };
 
 module.exports = function(grunt) {
-
-
-	var templates = {};
-	Object.keys( pages ).forEach(function( key ){
-		templates[key] = makePage( key, pages[key] );
-	});
-
-	// Project configuration.
 	grunt.initConfig({
-		pkg: '<json:package.json>',
-		meta: {
-			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-		},
-		jshint: {
-			all: ['Gruntfile.js', 'app/static/js/app/*.js', 'app/static/js/app/**/*.js'],
-			options: {
-				eqeqeq: true,
-				immed: true,
-				latedef: false,
-				regexdash: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				boss: true,
-				eqnull: true,
-				globals: {
-					jQuery: true,
-					define: true,
-					require: true,
-					window: true,
-					console: true,
-					document: true
-				}
-			}
-		},
+		// compile .scss/.sass to .css using Compass
+		// http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
 		compass: {
 			dist: {
 				options: {
-					basePath: 'app/',
-                    cssDir: 'static/css',
-                    sassDir: 'static/css/src',
-                    imagesDir: 'static/images',
-                    outputStyle: isProduction ? 'compressed' : 'compact',
-                    noLineComments: isProduction
+					cssDir: 'public/static/css',
+					sassDir: 'static/css/src'
 				}
 			}
 		},
-		template: templates,
-		//regarde is watch but fancier
-		regarde: {
-			compass: {
-				files: [
-					'app/static/css/src/*.{scss,sass}'
-				],
-				tasks: ['compass']
-			},
-			template: {
-				files: [ 'app/templates/*.html'],
-				tasks: ['template']
+		// specifying JSHint options and globals
+		// https://github.com/cowboy/grunt/blob/master/docs/task_lint.md#specifying-jshint-options-and-globals
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'public javascripts/*.js',
+				'public/javascripts/**/*.js',
+				//ignore vendor libraries
+				'!public/javascripts/vendor/*.js',
+				'!public/javascripts/vendor/**/*.js'
+			],
+			options: {
+				curly: true, //use curly braces even on one-liners
+				eqeqeq: true, //use strict equality (===, !==)
+				immed: true, //wrap self-invoking functions in parentheses
+				newcap: true, //capitalize first letter of all constructor functions (i.e. new MyObject())
+				noarg: true, //prohibit the use of arguments.caller & arguments.callee (which are forbidden in strict mode of es5+)
+				sub: true, //supress warnings for using brace syntax instead of dot syntax on objects
+				undef: true, //prohibit the use of undeclared variables (spot leaks + globals)
+				eqnull: true, //suppress warnings about using "== null"
+				browser: true, //define globals exposed by modern browsers
+				globals: {
+					module: true, //commonjs global
+					define: true, //AMD global
+					require: true, //AMD + CommonJS global
+					console: true //turn this to false when supporting real-old browsers
+				}
 			}
 		},
+		//compile requirejs files for production
 		requirejs: {
 			compile: {
 				options: {
-					appDir: 'app/',
 					baseUrl: 'static/js',
-					dir: './production',
+					dir: 'public/static/js',
 					optimizeCss: 'standard',
-					findNestedDependencies: true,
+					//optimize: 'none',
+					//findNestedDependencies: true,
 					preserveLicenseComments: false,
-					pragmas: {
-						hostedBySunovian: true
-					},
-						//re-route libs to top-level
-					paths: {
-						'templates': '../templates',
-					},
-					modules: [
-						{
-							name: 'main'
+					//re-route libs to top-level
+					paths: {},
+					modules: [{
+							name: 'app/main',
+							exclude: [
+							]
 						}
 					]
+				}
+			}
+		},
+		//better-than-watch
+		watch: {
+			options: {
+				livereload: true
+			},
+			stylesheets: {
+				files: [
+						'static/css/src/*.{scss,sass}'
+				],
+				tasks: 'compass'
+			},
+			views: {
+				files: [
+						'views/*'
+				],
+				tasks: 'template'
+			}
+		},
+		//watch templates using consolidated.js
+		//setup for jade but can use many templating engines: https://github.com/rockwood/grunt-templater#supported-template-engines
+		template: {
+			index: {
+				src: 'views/index.dust.html',
+				dest: 'public/index.html',
+				engine: 'dust',
+				variables: {
+					title: "this is a title",
+					pretty: true,
+					views: './views'
 				}
 			}
 		},
@@ -142,23 +99,17 @@ module.exports = function(grunt) {
 			server: {
 				options: {
 					port: 8000,
-					base: 'app',
+					base: 'public',
 					keepalive: true
 				}
 			}
 		}
 	});
-	
-	grunt.loadNpmTasks('grunt-regarde');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
-	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-templater');
-	grunt.registerTask('default', ['template', 'compass', 'regarde']);
-	grunt.registerTask('watch', ['regarde']);
-	grunt.registerTask('production', ['template', 'compass', 'requirejs']);
-
+	grunt.registerTask('default', ['compass', 'template', 'jshint']);
 };
-
-
